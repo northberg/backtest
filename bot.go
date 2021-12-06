978a5b40492a36630ce9d5bb83238cdd8db2a785
aelbrecht <aelbrechtrudolf@gmail.com>
2021-12-06 15:41:20 +0100

    add version to bot instance

--- bot.go
+++ bot.go
@@ -8,13 +8,19 @@ type BotConfig struct {
 	Leverage   int       `json:"leverage"`
 }
 
+type BotVersion struct {
+	Id          string `json:"id"`
+	Description string `json:"description"`
+}
+
 type BotInstance struct {
-	Id        string `json:"id"`
-	Name      string `json:"name"`
-	CreatedOn int64  `json:"createdOn"`
-	StartTime int64  `json:"startTime"`
-	StopTime  int64  `json:"stopTime"`
-	Status    string `json:"status"`
-	Log       string `json:"log"`
-	Error     string `json:"error"`
+	Id        string     `json:"id"`
+	Version   BotVersion `json:"version"`
+	Name      string     `json:"name"`
+	CreatedOn int64      `json:"createdOn"`
+	StartTime int64      `json:"startTime"`
+	StopTime  int64      `json:"stopTime"`
+	Status    string     `json:"status"`
+	Log       string     `json:"log"`
+	Error     string     `json:"error"`
 }
