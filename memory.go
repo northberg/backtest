1fae678a2c1910746af9049cbbb69ce3deb1cd5b
aelbrecht <aelbrechtrudolf@gmail.com>
2021-12-17 11:17:49 +0100

    make memory serializable

--- memory.go
+++ memory.go
@@ -1,43 +1,43 @@
 package backtest
 
 type Memory struct {
-	storeFloat map[string]float64
-	storeInt   map[string]int64
+	Floats   map[string]float64 `json:"floats"`
+	Integers map[string]int64   `json:"integers"`
 }
 
 func NewMemory() *Memory {
 	return &Memory{
-		storeFloat: make(map[string]float64),
-		storeInt:   make(map[string]int64),
+		Floats:   make(map[string]float64),
+		Integers: make(map[string]int64),
 	}
 }
 
 func (m *Memory) StoreInt64(key string, v int64) {
-	m.storeInt[key] = v
+	m.Integers[key] = v
 }
 
 func (m *Memory) ReadInt64(key string) int64 {
-	return m.storeInt[key]
+	return m.Integers[key]
 }
 
 func (m *Memory) Store(key string, v int) {
-	m.storeInt[key] = int64(v)
+	m.Integers[key] = int64(v)
 }
 
 func (m *Memory) Read(key string) int {
-	return int(m.storeInt[key])
+	return int(m.Integers[key])
 }
 
 func (m *Memory) StoreBool(key string, v bool) {
 	if v {
-		m.storeInt[key] = 1
+		m.Integers[key] = 1
 	} else {
-		m.storeInt[key] = 0
+		m.Integers[key] = 0
 	}
 }
 
 func (m *Memory) ReadBool(key string) bool {
-	if m.storeInt[key] == 0 {
+	if m.Integers[key] == 0 {
 		return false
 	} else {
 		return true
@@ -45,9 +45,9 @@ func (m *Memory) ReadBool(key string) bool {
 }
 
 func (m *Memory) StoreFloat(key string, v float64) {
-	m.storeFloat[key] = v
+	m.Floats[key] = v
 }
 
 func (m *Memory) ReadFloat(key string) float64 {
-	return m.storeFloat[key]
+	return m.Floats[key]
 }
