b0b4e311bdee5e54292d9bc2c6434f417bef924d
aelbrecht <aelbrechtrudolf@gmail.com>
2021-12-17 10:59:09 +0100

    add trade state type

--- bot.go
+++ bot.go
@@ -1,5 +1,7 @@
 package backtest
 
+import cs "github.com/northberg/candlestick"
+
 type BotConfig struct {
 	Version    BotVersion `json:"version"`
 	Funds      float64    `json:"funds"`
@@ -24,4 +26,17 @@ type BotInstance struct {
 	Status    string `json:"status"`
 	Log       string `json:"log"`
 	Error     string `json:"error"`
+}
+
+type TradeState struct {
+	InstanceId string            `json:"instanceId"`
+	StateId    string            `json:"stateId"`
+	Balance    float64           `json:"balance"`
+	Funds      float64           `json:"funds"`
+	Symbol     string            `json:"symbol"`
+	Time       int64             `json:"time"`
+	Parameters []float64         `json:"parameters"`
+	Memory     Memory            `json:"memory"`
+	Leverage   int               `json:"leverage"`
+	Position   cs.HedgedPosition `json:"position"`
 }
